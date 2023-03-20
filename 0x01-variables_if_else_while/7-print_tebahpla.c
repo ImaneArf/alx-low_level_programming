43f597ee713f6c7826718b408a5ce09cd5cedb05
ArfImane <arf.imane@gmail.com>
2023-03-20 11:54:54 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'Z'; ch <= 'A' ; ch--)
+	{
+		putchar(tolower(ch));
+	}
+	putchar('\n');
+	return (0);
+}
