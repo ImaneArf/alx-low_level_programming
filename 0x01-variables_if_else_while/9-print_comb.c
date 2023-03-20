9d78e3769333b614ac992b5ee62934a4a9ada56a
ArfImane <arf.imane@gmail.com>
2023-03-20 12:20:07 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - main fuction
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int digit = 0;
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
