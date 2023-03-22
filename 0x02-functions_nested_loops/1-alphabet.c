368198318ec3c4600e98139e97ca099779ee6701
ArfImane <arf.imane@gmail.com>
2023-03-22 10:48:51 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabets
+ * print_alphabet- utilizes to print all alphabets
+ *
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
