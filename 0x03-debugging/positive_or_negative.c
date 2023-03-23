89be0c38b7766213571d46d52db3966ef76edf3b
ArfImane <arf.imane@gmail.com>
2023-03-23 15:50:23 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,24 +1,14 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
-#include "main.h"
 /**
- * main - main function
- *
- * Return: always 0
+ * positive_or_negative - function to check if n is pos or neg
+ * @n: param
  */
 void positive_or_negative(int n)
 {
 	if (n < 0)
-	{
 		printf("%d is negative\n", n);
-	}
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
 	else
-	{
 		printf("%d is positive\n", n);
-	}
 }
