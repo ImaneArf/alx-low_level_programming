9b87c5149f69e9ada5aaf4bae4b0d8ab472a68f9
ArfImane <arf.imane@gmail.com>
2023-03-23 15:40:09 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
  * main - function to test if the number is positive or negative
  *
  * Return: always 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 98 % 2;
 	positive_or_negative(i);
+
 	return (0);
 }
